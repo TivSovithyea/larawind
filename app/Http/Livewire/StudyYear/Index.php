08e2b50cf049585103829b1f31e23f626bd61b17
Tiv Sovithyea <sovithyeativ@gmail.com>
2023-03-02 19:16:12 +0700

    improve teacher

--- app/Http/Livewire/StudyYear/Index.php
+++ app/Http/Livewire/StudyYear/Index.php
@@ -2,12 +2,67 @@
 
 namespace App\Http\Livewire\StudyYear;
 
+use App\Models\StudyYear;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    public $name, $start_date, $end_date, $study_year_id, $search;
+
     public function render()
     {
-        return view('livewire.study-year.index');
+        $data = DB::table('study_years')
+            ->orWhere('start_date', 'LIKE', '%' . $this->search . '%')
+            ->orWhere('end_date', 'LIKE', '%' . $this->search . '%')
+            ->orWhere('name', 'LIKE', '%' . $this->search . '%')
+            ->paginate(5);
+        return view('livewire.study-year.index', compact('data'))->layout('layouts.app-livewire');
+    }
+
+    public function onEdit($id)
+    {
+        $study_year = StudyYear::findOrFail($id);
+        $this->name = $study_year->name;
+        $this->start_date = $study_year->start_date;
+        $this->end_date = $study_year->end_date;
+        $this->study_year_id = $id;
+    }
+
+    public function onSave()
+    {
+        if ($this->study_year_id) {
+            $study_year = StudyYear::findOrFail($this->study_year_id);
+            $study_year->name = $this->name;
+            $study_year->start_date = $this->start_date;
+            $study_year->end_date = $this->end_date;
+        } else {
+            $study_year = new StudyYear();
+            $study_year->name = $this->name;
+            $study_year->start_date = $this->start_date;
+            $study_year->end_date = $this->end_date;
+        }
+        if ($study_year->save()) {
+            $this->name = null;
+            $this->start_date = null;
+            $this->end_date = null;
+            $this->study_year_id = null;
+            //sucess
+        } else {
+            //error
+        }
+    }
+
+    public function onDelete($id)
+    {
+        $study_year = StudyYear::findOrFail($this->study_year_id);
+        if ($study_year->delete()) {
+            //success
+        } else {
+            //error
+        }
     }
 }
--- app/Http/Livewire/Teacher/Index.php
+++ app/Http/Livewire/Teacher/Index.php
@@ -2,12 +2,75 @@
 
 namespace App\Http\Livewire\Teacher;
 
+use App\Models\Subject;
+use App\Models\Teacher;
 use Livewire\Component;
 
 class Index extends Component
 {
+
+    public $subjects = [];
+    public $code, $name, $name_latin, $gender, $dob, $phone, $address;
+    public $teacher_subjects, $teacher_id;
     public function render()
     {
-        return view('livewire.teacher.index');
+
+        $this->subjects = Subject::all();
+
+        $data = Teacher::latest()->paginate(10);
+        return view('livewire.teacher.index', compact('data'))->layout('layouts.app-livewire');
+    }
+
+    public function onEdit($id)
+    {
+        $teacher = Teacher::findOrFail($id);
+        $this->code = $teacher->code;
+        $this->name = $teacher->name;
+        $this->name_latin = $teacher->name_latin;
+        $this->gender = $teacher->gender;
+        $this->dob = $teacher->dob;
+        $this->phone = $teacher->phone;
+        $this->address = $teacher->address;
+        $this->teacher_subjects = $teacher->subject->pluck('id');
+        $this->teacher_id = $teacher->id;
+    }
+
+    public function onSave()
+    {
+        if ($this->teacher_id) {
+            $teacher = Teacher::findOrFail($this->teacher_id);
+            $teacher->code = $this->code;
+            $teacher->name = $this->name;
+            $teacher->name_latin = $this->name_latin;
+            $teacher->gender = $this->gender;
+            $teacher->dob = $this->dob;
+            $teacher->phone = $this->phone;
+            $teacher->address = $this->address;
+            $teacher->subjects->sync($this->teacher_subjects);
+        } else {
+            $teacher = new Teacher();
+            $teacher->code = $this->code;
+            $teacher->name = $this->name;
+            $teacher->name_latin = $this->name_latin;
+            $teacher->gender = $this->gender;
+            $teacher->dob = $this->dob;
+            $teacher->phone = $this->phone;
+            $teacher->address = $this->address;
+            $teacher->subjects->attact($this->teacher_subjects);
+        }
+        if ($teacher->save()) {
+            $this->code = null;
+            $this->name = null;
+            $this->name_latin = null;
+            $this->gender = null;
+            $this->dob = null;
+            $this->phone = null;
+            $this->address = null;
+            $this->teacher_subjects = null;
+            $this->teacher_id = null;
+            //success
+        } else {
+            //error
+        }
     }
 }
